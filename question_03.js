a6ab898775aaa3b735324aa974044d313392bd9d
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-19 23:29:05 +0500

    let var const all question added

--- question_03.js
+++ question_03.js
@@ -1,5 +1,7 @@
 // Write a loop that iterates over an array of strings and prints the first letter of each
 // string. Use let to declare a block-scoped variable inside the loop.
-function printString(){
-    
+let strArr=["Hafsa","Sarim","Zahra","Mawiya","Taha","Haider","Malaika"];
+for(const alpha of strArr){
+    let strLet=alpha.charAt(0);
+    console.log(strLet);
 }
--- question_04.js
+++ question_04.js
@@ -1,2 +1,12 @@
 // Write a function that takes a number n and returns a string with n asterisks. Use const
 // to declare a constant string that holds one asterisk.
+function pattern(num){
+    let symb="*";
+    let final="";
+    for(let i=0;i<num.length;i++){
+        final=final+symb
+    }
+    return final;
+}
+const num=5
+console.log(pattern(num));
